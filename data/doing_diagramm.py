0ccad92ddf0dff2e17c6a822da3ea7731a0a6e8e
Egor <e.v.schukin@yandex.ru>
2021-04-22 09:07:58 +0300

    Сломали базу данных вот теперь чиним

--- data/doing_diagramm.py
+++ data/doing_diagramm.py
@@ -4,6 +4,7 @@ from .db_session import DataBase
 
 
 class DoingDiagram(DataBase):
+    __table_args__ = {'extend_existing': True}
     __tablename__ = 'diagram'
 
     id = sql.Column(sql.Integer, primary_key=True, index=True, unique=False, nullable=False)
